e4641a0b030587297ff661443836e019f8e88f70
Eliel Assis <eliel.assis7@hotmail.com>
2023-10-26 19:48:38 -0300

    estilos globais

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
+import { globalStyle } from '../styles/global'
+
+globalStyle()
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
+import { styled } from '../styles'
 
+const Button = styled('button', {
+  backgroundColor: '$green300',
+  borderRadius: 8,
+  border: 0,
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold',
+  },
+  '&:hover': {
+    filter: 'brightness(0.8)',
+  },
+})
 
 export default function Home() {
   return (
-    <h1>Hello Wolrd</h1>
+    <Button>
+      <span>Teste </span>
+      Enviar
+    </Button>
   )
 }
